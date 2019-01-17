8920db7626ea1e5dc25cc0d3a6848b395765595d
Elle H <itsellej@gmail.com>
2019-01-17 14:44:04 +0000

    Create server.js and webpack.prod.js. Install dotenv, dotenv-webpack, express node-fetch. Add .env to git ignore.  Add start and heroku-postbuild to scripts and engines  to package.json

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const port = process.env.PORT || 8080;
+const app = express();
+
+if (process.env.NODE_ENV == 'development') require('dotenv').config({ silent: true });
+
+app.use(express.static(__dirname + '/dist'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './index.html'));
+});
+
+app.listen(port);

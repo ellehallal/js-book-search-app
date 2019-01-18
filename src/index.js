6b74821479f9cf127229240ec0d7c843e6aa70fe
Elle Hallal <daniellehallal@gmail.com>
2019-01-18 18:20:29 +0000

    create displaySearchResults() to display book data

--- src/index.js
+++ src/index.js
@@ -5,10 +5,35 @@ const bookSearchData = new BookSearchData();
 
 const submit = document.getElementById('submit');
 const submitQuery = document.getElementById('submit-query');
+const searchResults = document.getElementById('search-results')
 
 submit.addEventListener('click', async function (){
   console.log('hello')
   const searchFieldInput = document.getElementById('search-field').value;
   const results = await bookSearchData.returnSortedData(searchFieldInput)
-  console.log(results)
+  return displaySearchResults(results)
 })
+
+function displaySearchResults(results) {
+  results.forEach((book) => {
+    const bookData = document.createElement('div');
+    const title = document.createElement('p');
+    const author = document.createElement('p');
+    const publisher = document.createElement('p');
+    const rating = document.createElement('p');
+    const link = document.createElement('p');
+    const image = document.createElement('img');
+
+    title.innerHTML = `Title: ${book.title}`;
+    author.innerHTML = `Author(s): ${book.author}`;
+    publisher.innerHTML = `Publisher ${book.publisher}`;
+    rating.innerHTML = `Rating: ${book.rating}`;
+    link.innerHTML = book.link
+    image.src = book.image
+
+    bookData.append(title,author,publisher,rating,link,image);
+    console.log(bookData)
+    searchResults.appendChild(bookData)
+  })
+
+}

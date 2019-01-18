f9aebd3b6db3ecee243c84239746b17fe304b37a
Elle Hallal <daniellehallal@gmail.com>
2019-01-18 16:23:22 +0000

    Add event listener to submit button, which calls returnSortedData() in booksearchclass

--- src/index.js
+++ src/index.js
@@ -2,4 +2,13 @@ import { BookSearchData } from './book_search_data';
 
 const bookSearchData = new BookSearchData();
 
-const searchFieldInput = document.querySelector('#search-field > input')
+
+const submit = document.getElementById('submit');
+const submitQuery = document.getElementById('submit-query');
+
+submit.addEventListener('click', async function (){
+  console.log('hello')
+  const searchFieldInput = document.getElementById('search-field').value;
+  const results = await bookSearchData.returnSortedData(searchFieldInput)
+  console.log(results)
+})

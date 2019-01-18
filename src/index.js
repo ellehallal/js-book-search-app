2a183ffae789e46b522a332b20d5ac9557e25cc2
Elle Hallal <daniellehallal@gmail.com>
2019-01-18 14:43:54 +0000

    Add input field for search query and submit button

--- src/index.js
+++ src/index.js
@@ -1,48 +1,5 @@
-import { FormatData } from './format_data';
+import { BookSearchData } from './book_search_data';
 
-const numbers = new Numbers();
-const radix = 10;
-let listOfNumbers = [];
+const bookSearchData = new BookSearchData();
 
-function otherWayOfWritingText() {
-  const element = document.createElement('p');
-  element.innerHTML = 'Or I can put it in via a function!';
-  return element;
-}
-
-function temporarilyClearNumbers() {
-  document.getElementById('list-of-numbers').innerHTML = '';
-}
-
-function displayList() {
-  temporarilyClearNumbers();
-  listOfNumbers.forEach((number) => {
-    const element = document.createElement('li');
-    element.innerHTML = number;
-    document.getElementById('list-of-numbers').appendChild(element);
-  });
-}
-
-function addNumberToList(event) {
-  event.preventDefault();
-  const number = document.getElementById('number').value;
-  listOfNumbers.push(parseInt(number, radix));
-  displayList();
-}
-
-function addOneToEachNumber() {
-  listOfNumbers = numbers.addOneToAll(listOfNumbers);
-  displayList();
-}
-
-
-function clearNumbers() {
-  listOfNumbers = [];
-  displayList();
-}
-
-document.getElementById('other-way-of-writing-text').appendChild(otherWayOfWritingText());
-document.getElementById('add-number').onclick = addNumberToList;
-document.getElementById('add-one-to-each-number').onclick = addOneToEachNumber;
-document.getElementById('temporarily-clear-numbers').onclick = temporarilyClearNumbers;
-document.getElementById('clear-numbers').onclick = clearNumbers;
+const searchFieldInput = document.querySelector('#search-field > input')

d922fe07b7e544f0b60477ababdc521ee852de20
Elle Hallal <daniellehallal@gmail.com>
2019-01-18 09:04:51 +0000

    Mock API call for test: 'tests the getSearchResultData function'

--- __tests__/api_call_test.js
+++ __tests__/api_call_test.js
@@ -2,16 +2,18 @@ const fetch = require('node-fetch');
 
 import { APICall } from '../src/api_call';
 
+jest.mock("../src/api_call");
+
 describe('API Call', () => {
 
   describe('get search result data', () => {
     const apiCall = new APICall();
 
     it('tests the getSearchResultData function', async () => {
-      const data = await apiCall.getSearchResultData("Harry Potter");
-      console.log(data.kind)
-      expect(data.kind).toEqual('books#volumes')
+      await apiCall.getSearchResultData("Harry Potter");
+      expect(apiCall.getSearchResultData).toHaveBeenCalled()
+      expect(apiCall.getSearchResultData).toHaveBeenCalledTimes(1)
+      expect(apiCall.getSearchResultData).toHaveBeenCalledWith("Harry Potter")
     });
-
   });
 });
